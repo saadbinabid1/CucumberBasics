eef5a76f1ccd2b406c7dcb5cd9b27f9aa96a5484
saadbinabid1 <abid@fortiss.org>
2019-11-04 01:47:32 +0100

    fixed the problem with loading report1.xml in RunCucumberTest.java

--- src/test/java/CucumberDemo/RunUserTest.java
+++ src/test/java/CucumberDemo/RunUserTest.java
@@ -21,6 +21,7 @@ public class RunUserTest {
         public static void writeExtentReport()
         {
             // solved the pie chart issue in the reporting via the following url
+            // using the command in so jenkins console: System.setProperty("hudson.model.DirectoryBrowserSupport.CSP", "default-src * 'unsafe-inline' 'unsafe-eval'; script-src * 'unsafe-inline' 'unsafe-eval'; connect-src * 'unsafe-inline'; img-src * data: blob: 'unsafe-inline'; frame-src *; style-src * 'unsafe-inline';")
             //https://stackoverflow.com/questions/35783964/jenkins-html-publisher-plugin-no-css-is-displayed-when-report-is-viewed-in-j/35785788#35785788
                Reporter.loadXMLConfig(new File("config/report.xml"));
         }
